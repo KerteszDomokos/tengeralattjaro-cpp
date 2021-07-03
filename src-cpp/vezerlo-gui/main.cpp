d181d7f81a832bb3445944aebbf7c05714b63b43
Kertész Domokos <kerteszdomokos@gmail.com>
2021-07-03 17:28:03 +0200

    KOmmunikáció eltávolítás 2

--- src-cpp/vezerlo-gui/main.cpp
+++ src-cpp/vezerlo-gui/main.cpp
@@ -1,6 +1,5 @@
 #define PY_SSIZE_T_CLEAN
 #include "gui.h"
-#include "kommunikacio.h"
 
 #include <QApplication>
 #include <windows.h>
@@ -15,9 +14,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     GUI w;
     w.show();
-    Kommunikacio th;
-    th.start();
-    qDebug() << "hello from GUI thread " << QThread::currentThread();
+
     return a.exec();
 }
 

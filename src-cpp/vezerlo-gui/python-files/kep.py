5d17cff25e86382c02b8746990be5efc0f45c1f7
Kertész Domokos <kerteszdomokos@gmail.com>
2021-10-19 22:41:02 +0200

    Kép továbbítás IP - vezérlés

--- src-cpp/vezerlo-gui/python-files/kep.py
+++ src-cpp/vezerlo-gui/python-files/kep.py
@@ -10,7 +10,7 @@ def kep():
     MAX_DGRAM = 2**16
     global dat
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.bind(('192.168.31.171', 5556))
+    s.bind(('169.254.62.249', 5556)) #eszköz ip
     s.settimeout(5)
     dat = b''
     while True:

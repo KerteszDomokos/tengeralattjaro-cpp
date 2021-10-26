2a41e9c94b50819564dd28ed3c5c91894c46b390
Kertész Domokos <kerteszdomokos@gmail.com>
2021-10-26 14:27:54 +0200

    Módbeállítások
    Egyéb mód választásánál inaktiválás
    Alapértelmezett módok kiválasztása
    Aktiválás a tesztfelületen

--- src-cpp/vezerlo-gui/settings.cpp
+++ src-cpp/vezerlo-gui/settings.cpp
@@ -7,6 +7,7 @@ settings::settings(QWidget *parent) :
     ui(new Ui::settings)
 {
     ui->setupUi(this);
+    otherThema();
     ui->qssszabvany->setOpenExternalLinks(1);
 }
 
@@ -17,16 +18,51 @@ settings::~settings()
 
 void settings::chooseFile()
 {
-    fileName = QFileDialog::getOpenFileName(this,tr("Megnyitás"), fileName,tr("Aqualab fájlok (*.al, *.AL, *.*)"));
+    fileName = QFileDialog::getOpenFileName(this,tr("Megnyitás"), fileName,tr("Aqualab fájlok (*.qss, *.QSS)"));
 
     ui->filename_tema->setText(fileName);
 
-    msg("Lejátszásnál kiválasztott fájlnevet lekérheti a parancssorból",1);
+    msg(tr("Lejátszásnál kiválasztott fájlnevet lekérheti a parancssorból"),1);
     QFile file(fileName);
     file.open(QFile::ReadOnly);
     QString ss = QString(file.readAll());
     file.close();
     ui->temateszt->setStyleSheet(ss);
+}
+
+void settings::otherThema()
+{
+    if(ui->masiktema->isChecked()==1){
+        ui->filename_tema->setEnabled(1);
+        ui->filevalasztas->setEnabled(1);
+        ui->qssszabvany->setEnabled(1);
+        ui->temavalaszto->setEnabled(0);
+        ui->deftema_label->setEnabled(0);
+    }
+    else{
+        ui->filename_tema->setEnabled(0);
+        ui->filevalasztas->setEnabled(0);
+        ui->qssszabvany->setEnabled(0);
+        ui->temavalaszto->setEnabled(1);
+        ui->deftema_label->setEnabled(1);
+    }
+}
+
+void settings::modvalasztas(QString mod)
+{
+    QString ss;
+    if(mod=="Világos mód"|| mod=="Light mode"){
+        ss="";
+    }
+    else if(mod=="Sötét mód" || mod=="Dark mode"){
+        QFile file(":/programs/darkmode");
+        file.open(QFile::ReadOnly);
+        ss = QString(file.readAll());
+        file.close();
+    }
+
+    ui->temateszt->setStyleSheet(ss);
+
 }
 
 QString settings::ujfileName()
--- src-cpp/vezerlo-gui/settings.h
+++ src-cpp/vezerlo-gui/settings.h
@@ -19,6 +19,8 @@ public:
 
 private slots:
     void chooseFile();
+    void otherThema();
+    void modvalasztas(QString mod);
 signals:
     void msg(QString dat, int pr);
 

47cfc09e15b44a96862186df31d6087978ab2459
KertészDomokos <kerteszdomokos@gmail.com>
2022-02-18 20:00:09 +0100

    Joystick MSI javítás

--- src-cpp/vezerlo-gui/python-files/joystick.py
+++ src-cpp/vezerlo-gui/python-files/joystick.py
@@ -1,5 +1,4 @@
 import struct
-import cv2
 import pygame
 import sys
 import os
@@ -29,11 +28,12 @@ def joy():
     while jo==1:
         try:
             pygame.event.get()
-            txt=str(j.get_axis(0))+' '+str(j.get_axis(1))+' '+str(j.get_axis(3))+' '+str(j.get_button(0))+' '+str(j.get_button(1))+' '+str(j.get_button(2))+' '+str(j.get_button(3))+' '+str(j.get_button(4))+' '+str(j.get_button(5))+' '+str(j.get_button(6))+' '+str(j.get_button(7))+' '+str(j.get_button(8))+' '+str(j.get_button(9))+' '+str(j.get_button(10))+' '+str(j.get_button(11))
+            txt=str(j.get_axis(0))+' '+str(j.get_axis(1))+' '+str(j.get_axis(4))+' '+str(j.get_button(0))+' '+str(j.get_button(1))+' '+str(j.get_button(2))+' '+str(j.get_button(3))+' '+str(j.get_button(4))+' '+str(j.get_button(5))+' '+str(j.get_button(6))+' '+str(j.get_button(7))+' '+str(j.get_button(8))+' '+str(j.get_button(9))+' '+str(j.get_button(10))+' '+str(j.get_button(11))
             f=open(file_path.replace("joystick.py","../program-datas/joystick.txt"),'w')
             f.write(txt)
             f.close()
             sleep(.005)
+            print(txt)
         except KeyboardInterrupt:
             print("Kilépés: KeyboardInterrupt")
             f.close()
